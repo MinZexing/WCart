0643299d06248ab7150f2374e1d7e89c2453a3e1
MinZexing <zexing1908@gmail.com>
2025-08-15 00:24:43 +1000

    create login and signup component for the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,18 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
+import AllProducts from "./pages/AllProducts";
 
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller");
+  const { showUserLogin } = useAppContext();
 
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
 
       <Toaster />
       <div
@@ -17,6 +22,7 @@ function App() {
       >
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
